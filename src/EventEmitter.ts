a999e5284d60fc4d8fa73e8f175f20bf030d26df
Ariel Saldana <ariel@unreal.io>
2022-10-29 21:34:25 -0400

    Add EventEmitter class

--- src/EventEmitter.ts
+++ src/EventEmitter.ts
@@ -1,10 +1,22 @@
 export class EventEmitter {
-    addEvent(): void {
+    eventMap = new Map();
+
+    emit(eventKey: string, ...args): void {
+        if (this.eventMap.has(eventKey)) {
+            this.eventMap.get(eventKey).forEach((value) => {
+                value(args);
+            });
+        }
     }
 
-    removeEvent(): void {
-    }
-
-    emit(): void {
+    on(eventKey: Object, eventCallback: Object): void {
+        let arr;
+        if (this.eventMap.get(eventKey)) {
+            arr = this.eventMap.get(eventKey)
+        } else {
+            arr = Array()
+            this.eventMap.set(eventKey, arr)
+        }
+        arr.push(eventCallback);
     }
 }
