47d5b67066c6d8f2180caf8bdd8b98192cae0725
Ariel Saldana <ariel@unreal.io>
2022-12-13 17:05:39 -0500

    Screen Event

--- src/tools/Screen.ts
+++ src/tools/Screen.ts
@@ -1,5 +1,47 @@
 import { EventEmitter } from '../Pan'
 
 export default class Screen extends EventEmitter {
-    beforeOnEventListenerSetup (eventKey: string): void {}
+    settings = {
+        isScreenResizeEnabled: false
+    }
+
+    events = {
+        resize: {
+            create: () => { this.registerScreenResizeListener() },
+            destroy: () => { this.destroyScreenResizeListener() }
+        }
+    }
+
+    screenResize(eventInformation): void {
+        const location = {
+            width: window.innerWidth,
+            height: window.innerHeight
+        }
+        this.emit('resize', location)
+    }
+
+    beforeOnEventListenerSetup (eventKey: string): void {
+        if (this.events[eventKey] !== undefined) {
+            this.registerEventListenerFunction(eventKey, {
+                initFunction: this.events[eventKey].create.bind(this),
+                destroyFunction: this.events[eventKey].destroy.bind(this)
+            })
+        } else {
+            console.warn('This is not a supported Event')
+        }
+    }
+
+    registerScreenResizeListener(): void {
+        if (!this.settings.isScreenResizeEnabled) {
+            window.addEventListener('resize', (ev) => {
+                this.screenResize(ev)
+            })
+            this.settings.isScreenResizeEnabled = true
+        }
+    }
+
+    destroyScreenResizeListener(): void {
+        window.removeEventListener('resize', this.screenResize)
+        this.settings.isScreenResizeEnabled = false
+    }
 }
