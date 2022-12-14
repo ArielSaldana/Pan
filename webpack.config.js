86736f388c86ad20459ca588fc8310b911d378f9
Ariel Saldana <ariel@unreal.io>
2022-12-14 11:53:52 -0500

    Update project

--- webpack.config.js
+++ webpack.config.js
@@ -1,16 +1,16 @@
 // Generated using webpack-cli https://github.com/webpack/webpack-cli
 
-const path = require('path');
+const path = require('path')
 
-const isProduction = process.env.NODE_ENV == 'production';
+const isProduction = process.env.NODE_ENV === 'production'
 
 const config = {
     entry: './src/Pan.ts',
     output: {
         path: path.resolve(__dirname, 'dist'),
         library: 'Pan',
-        libraryTarget:'var',
-        filename: 'pan.js',
+        libraryTarget: 'var',
+        filename: 'pan.js'
     },
     plugins: [
         // Add your plugins here
@@ -21,28 +21,26 @@ const config = {
             {
                 test: /\.(ts|tsx)$/i,
                 loader: 'ts-loader',
-                exclude: ['/node_modules/'],
+                exclude: ['/node_modules/']
             },
             {
                 test: /\.(eot|svg|ttf|woff|woff2|png|jpg|gif)$/i,
-                type: 'asset',
-            },
+                type: 'asset'
+            }
             // Add your rules for custom modules here
             // Learn more about loaders from https://webpack.js.org/loaders/
-        ],
+        ]
     },
     resolve: {
-        extensions: ['.tsx', '.ts', '.jsx', '.js', '...'],
-    },
-};
+        extensions: ['.tsx', '.ts', '.jsx', '.js', '...']
+    }
+}
 
 module.exports = () => {
     if (isProduction) {
-        config.mode = 'production';
-
-
+        config.mode = 'production'
     } else {
-        config.mode = 'development';
+        config.mode = 'development'
     }
-    return config;
-};
+    return config
+}
