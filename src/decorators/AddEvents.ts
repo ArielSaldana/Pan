eb14219c8484d9321666b1958b2a72bceab520af
Ariel Saldana <ariel@unreal.io>
2023-01-01 09:04:41 -0500

    create addEvent utility decorator

---
+++ src/decorators/AddEvents.ts
@@ -0,0 +1,18 @@
+export default function addEvent(eventName, klazz): any {
+    return (target: any, propertyKey: string, descriptor: PropertyDescriptor) => {
+        klazz.events.set(
+            eventName,
+            {
+                initFunction: () => { document.addEventListener(eventName, descriptor.value.bind(klazz)) },
+                destroyFunction: () => { document.removeEventListener(eventName, descriptor.value.bind(klazz)) },
+                callbacks: []
+            }
+        )
+
+        const original = descriptor.value
+        descriptor.value = function (...args) {
+            const result = original.call(this, ...args)
+            klazz.emit(eventName, result, args[0])
+        }
+    }
+}
