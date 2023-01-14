0d4bd11655c8c9405dccdfb34d0a3ec91ac55262
Ariel Saldana <ariel@unreal.io>
2023-01-13 22:40:26 -0500

    Updating ticker tool

--- src/tools/ticker/Ticker.ts
+++ src/tools/ticker/Ticker.ts
@@ -3,10 +3,23 @@ import { TickerState } from './TickerState'
 import TickerEventList from './TickerEventList'
 
 export default class Ticker extends EventEmitter {
+    static instance: Ticker
     state: TickerState = {
         hasTickerStarted: false,
         ticks: 0,
-        previousTickTimeStamp: 0
+        previousTickTimeStamp: 0,
+        tickProgress: Date.now()
+    }
+
+    private constructor () {
+        super()
+    }
+
+    static getInstance(): Ticker {
+        if (Ticker.instance === undefined) {
+            Ticker.instance = new Ticker()
+        }
+        return Ticker.instance
     }
 
     tickerEventList: TickerEventList = new TickerEventList()
@@ -28,42 +41,57 @@ export default class Ticker extends EventEmitter {
     tick(currentTime): void {
         const delta = currentTime - this.state.previousTickTimeStamp
         this.state.previousTickTimeStamp = currentTime
-        this.emit('tick', {
-            delta
-        })
 
-        const it = this.tickerEventList.getAllReadyToExecute()
-        let result = it.next()
-        while (result.done !== true) {
-            result.value.callback()
-            result = it.next()
+        if (delta < 100) {
+            this.state.ticks += delta
+            this.state.previousTickTimeStamp = currentTime
+            this.emit('tick', {
+                delta,
+                tick: this.getTick(),
+                tickRounded: Math.round(this.getTick())
+            })
+//            const it = this.tickerEventList.getAllReadyToExecute()
+//            let result = it.next()
+//            while (result.done !== true) {
+//                result.value.callback()
+//                result = it.next()
+//            }
+        } else {
+            console.log('frame skipped')
         }
         requestAnimationFrame(this.tick.bind(this))
     }
 
     /*
+     * Returns the ticks elapsed in ms
+     */
+    getTick(): number {
+        return this.state.ticks
+    }
+
+    /*
      * Takes in an argument in Seconds that defines how far in the future from now should the callback be called
      */
-    afterSeconds(elapsedTimeInSeconds: number, callback): void {
-        const executeBy = Date.now() + (elapsedTimeInSeconds * 1000)
-        this.at(executeBy, callback)
-    }
-
-    /*
-     * Takes in an argument in Milliseconds that defines how far in the future from now should the callback be called
-     */
-    after(elapsedTimeInMS: number, callback): void {
-        const executeBy = Date.now() + elapsedTimeInMS
-        this.at(executeBy, callback)
-    }
-
-    /*
-     * Gives a specific time to add to the TimeEventList, Denoted by UNIX EPOCH
-     */
-    at(executeBy: Number, callback): void {
-        this.tickerEventList.addNode({
-            executeBy,
-            callback
-        })
-    }
+//    afterSeconds(elapsedTimeInSeconds: number, callback): void {
+//        const executeBy = Date.now() + (elapsedTimeInSeconds * 1000)
+//        this.at(executeBy, callback)
+//    }
+//
+//    /*
+//     * Takes in an argument in Milliseconds that defines how far in the future from now should the callback be called
+//     */
+//    after(elapsedTimeInMS: number, callback): void {
+//        const executeBy = Date.now() + elapsedTimeInMS
+//        this.at(executeBy, callback)
+//    }
+//
+//    /*
+//     * Gives a specific time to add to the TimeEventList, Denoted by UNIX EPOCH
+//     */
+//    at(executeBy: Number, callback): void {
+//        this.tickerEventList.addNode({
+//            executeBy,
+//            callback
+//        })
+//    }
 }
