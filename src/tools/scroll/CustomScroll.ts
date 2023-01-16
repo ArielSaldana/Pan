7e6d31f43ba8c7177637e3872f5675d7b6fa5ebb
Ariel Saldana <ariel@unreal.io>
2023-01-15 22:38:26 -0500

    Add Custom Scroll Tool

---
+++ src/tools/scroll/CustomScroll.ts
@@ -0,0 +1,37 @@
+import Scroll from './Scroll'
+
+export default class CustomScroll {
+    scroll = Scroll.getInstance()
+    scrollElem: HTMLElement
+
+    state = {
+        scrollX: 0,
+        scrollY: 0
+    }
+
+    constructor () {
+        this.initScrollEventsListener()
+        this.hideDocumentBody()
+        this.scrollElem = this.getScrollElem()
+        this.scrollElem.style.overflow = 'auto'
+    }
+
+    hideDocumentBody(): void {
+        document.body.style.overflow = 'hidden'
+    }
+
+    getScrollElem(): HTMLElement {
+        const elem = document.querySelector('[data-pan-scroll]') as HTMLElement
+        if (elem !== undefined && elem !== null) {
+            return elem
+        }
+        throw Error('no element with data-pan-scroll found')
+    }
+
+    initScrollEventsListener(): void {
+        this.scroll.on('wheel', (wheelEvent) => {
+            console.log(wheelEvent)
+            this.scrollElem.style.transform = `matrix3d(1, 0, 0, 0, 0, 1, 0, 0, 0, 0, 1, 0, 0, ${wheelEvent.scrollY * -1}, 0, 1)`
+        })
+    }
+}
