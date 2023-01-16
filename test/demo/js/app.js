95bccc8b7d7aad637675da9411b82eb77a3e93aa
Ariel Saldana <ariel@unreal.io>
2023-01-15 22:50:00 -0500

    Update app.js

--- test/demo/js/app.js
+++ test/demo/js/app.js
@@ -69,106 +69,106 @@
         //        scroll.initCustomScroll()
 
         // Animation related work
-        const ticker = Pan.Ticker.getInstance()
-        let animationDuration = 1000
-
-        const ease = (timeElapsed) => {
-            if (timeElapsed >= animationDuration) {
-                animationState.done = true
-                return 100
-            }
-            if (!animationState.reverse) {
-                return Pan.easeOut(timeElapsed, 0, 100, animationDuration)
-            } else {
-                return Pan.reverseEaseOut(timeElapsed, 0, 100, animationDuration)
-            }
-
-//            return Pan.easeOut(timeElapsed, 0, 100, animationDuration)
-
-        }
-
-        const calculatePositionBasedOnProgress = (progress, viewportWidth, diff) => {
-            if (!animationState.reverse) {
-                return (progress) / 100
-            } else {
-                return (100 - progress ) / 100
-            }
-        }
-
-        const calculate = () => {
-            console.log('starTime: ', animationState.startTime)
-            console.log('progress:', animationState.progress)
-        }
-
-        const readjustAnimation = () => {
-            calculate()
-        }
-
-        const animationBox1 = document.getElementById('animation-box-1');
-
-        const animationState = {
-            startTime: 0,
-            progress: 0,
-            pos: 0,
-            paused: false,
-            done: false,
-            started: false,
-            reverse: false,
-            printChange: true
-        }
-
-        ticker.on('tick', (tickerEvent) => {
-            if (animationState.paused || !animationState.started || animationState.done) {
-                return
-            }
-            const elapsedTime = tickerEvent.tickRounded - animationState.startTime
-            updateState(elapsedTime)
-            render()
-        })
-
-        const updateState = (elapsedTime) => {
-            animationState.progress = ease(elapsedTime)
-            if (animationState.printChange) {
-                animationState.printChange = false
-                readjustAnimation()
-            }
-        }
-
-        const render = () => {
-            const diff = 130
-            animationState.pos = Math.round(calculatePositionBasedOnProgress(animationState.progress) * (viewportWidth - diff))
-            animationBox1.style.transform = `matrix3d(1, 0, 0, 0, 0, 1, 0, 0, 0, 0, 1, 0, ${animationState.pos}, 0, 0, 1)`
-        }
-
-        window.animationButtonToggle = () => {
-            if (animationState.done) {
-                animationState.done = false
-            }
-            animationState.paused = true
-            console.log('toggling')
-            if (!animationState.started) {
-                animationState.started = true
-            } else if (animationState.started) {
-                animationState.reverse = !animationState.reverse
-            }
-
-            animationState.startTime = Math.round(ticker.getTick())
-            animationState.printChange = true
-            animationState.paused = false
-        }
-
-
-        // calculate things
-        const testAnimationState = {
-            startTime: 0,
-            progress: 0,
-            pos: 0,
-            paused: false,
-            done: false,
-            started: false,
-            reverse: false,
-            printChange: true
-        }
+//        const ticker = Pan.Ticker.getInstance()
+//        let animationDuration = 1000
+//
+//        const ease = (timeElapsed) => {
+//            if (timeElapsed >= animationDuration) {
+//                animationState.done = true
+//                return 100
+//            }
+//            if (!animationState.reverse) {
+//                return Pan.easeOut(timeElapsed, 0, 100, animationDuration)
+//            } else {
+//                return Pan.reverseEaseOut(timeElapsed, 0, 100, animationDuration)
+//            }
+//
+////            return Pan.easeOut(timeElapsed, 0, 100, animationDuration)
+//
+//        }
+//
+//        const calculatePositionBasedOnProgress = (progress, viewportWidth, diff) => {
+//            if (!animationState.reverse) {
+//                return (progress) / 100
+//            } else {
+//                return (100 - progress ) / 100
+//            }
+//        }
+//
+//        const calculate = () => {
+//            console.log('starTime: ', animationState.startTime)
+//            console.log('progress:', animationState.progress)
+//        }
+//
+//        const readjustAnimation = () => {
+//            calculate()
+//        }
+//
+//        const animationBox1 = document.getElementById('animation-box-1');
+//
+//        const animationState = {
+//            startTime: 0,
+//            progress: 0,
+//            pos: 0,
+//            paused: false,
+//            done: false,
+//            started: false,
+//            reverse: false,
+//            printChange: true
+//        }
+//
+//        ticker.on('tick', (tickerEvent) => {
+//            if (animationState.paused || !animationState.started || animationState.done) {
+//                return
+//            }
+//            const elapsedTime = tickerEvent.tickRounded - animationState.startTime
+//            updateState(elapsedTime)
+//            render()
+//        })
+//
+//        const updateState = (elapsedTime) => {
+//            animationState.progress = ease(elapsedTime)
+//            if (animationState.printChange) {
+//                animationState.printChange = false
+//                readjustAnimation()
+//            }
+//        }
+//
+//        const render = () => {
+//            const diff = 130
+//            animationState.pos = Math.round(calculatePositionBasedOnProgress(animationState.progress) * (viewportWidth - diff))
+//            animationBox1.style.transform = `matrix3d(1, 0, 0, 0, 0, 1, 0, 0, 0, 0, 1, 0, ${animationState.pos}, 0, 0, 1)`
+//        }
+//
+//        window.animationButtonToggle = () => {
+//            if (animationState.done) {
+//                animationState.done = false
+//            }
+//            animationState.paused = true
+//            console.log('toggling')
+//            if (!animationState.started) {
+//                animationState.started = true
+//            } else if (animationState.started) {
+//                animationState.reverse = !animationState.reverse
+//            }
+//
+//            animationState.startTime = Math.round(ticker.getTick())
+//            animationState.printChange = true
+//            animationState.paused = false
+//        }
+//
+//
+//        // calculate things
+//        const testAnimationState = {
+//            startTime: 0,
+//            progress: 0,
+//            pos: 0,
+//            paused: false,
+//            done: false,
+//            started: false,
+//            reverse: false,
+//            printChange: true
+//        }
 
 //        console.log(ease(timeElapsed)); //150 ms out of 1000 has passed -> 27.75% pos is
 //        console.log(calculatePositionBasedOnProgress(27.75) * (viewportWidth - 130)); // -> 197.3025px
@@ -181,15 +181,15 @@
 //        console.log(Pan.easeOut(timeElapsed, 0, 100, animationDuration));
 //        console.log('time elapsed from function: ', Pan.easeOutElapsedTime(10, 0, 100, 1000))
 
-        const timeElapsed = 150
-        const easeOutValue = Pan.easeOut(timeElapsed, 0, 100, animationDuration)
-
-
-        const reverseEaseOutValue = Pan.reverseEaseOut(easeOutValue, 0, 100, animationDuration)
-
-        console.log('timeElapsed:', timeElapsed)
-        console.log('easeOutValue:', easeOutValue)
-        console.log('reverseEaseOutValue:', reverseEaseOutValue)
+//        const timeElapsed = 150
+//        const easeOutValue = Pan.easeOut(timeElapsed, 0, 100, animationDuration)
+//
+//
+//        const reverseEaseOutValue = Pan.reverseEaseOut(easeOutValue, 0, 100, animationDuration)
+//
+//        console.log('timeElapsed:', timeElapsed)
+//        console.log('easeOutValue:', easeOutValue)
+//        console.log('reverseEaseOutValue:', reverseEaseOutValue)
 
         // in order to achieve this we should adjust the start time to make the easing function start at that point
 
